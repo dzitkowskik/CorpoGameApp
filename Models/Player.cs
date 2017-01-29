0e00af4ee89d32145a26afbe3f78170b9d59e143
Karol Dzitkowski <k.dzitkowski@gmail.com>
2017-01-29 21:09:42 +0100

    add identity to Player Id

--- Models/Player.cs
+++ Models/Player.cs
@@ -1,12 +1,15 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CorpoGameApp.Models
 {
     public class Player
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
         public string Name { get; set; }
         public string Surname { get; set; }
         public string UserForeignKey { get; set;}
