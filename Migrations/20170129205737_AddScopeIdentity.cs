797eb9b7bde56a2baf63be3b68970be2e943895a
Karol Dzitkowski <k.dzitkowski@gmail.com>
2017-01-29 21:59:47 +0100

    add missing migration

---
+++ Migrations/20170129205737_AddScopeIdentity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CorpoGameApp.Migrations
+{
+    public partial class AddScopeIdentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
