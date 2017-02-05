1002a9ac4326794eae0252e82395356012255dc9
Karol Dzitkowski <k.dzitkowski@gmail.com>
2017-02-05 22:46:02 +0100

    start implementing account management

---
+++ ViewModels/Manage/ManageAccountViewModel.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CorpoGameApp.ViewModels.Manage
+{
+    public class ManageAccountViewModel
+    {
+        [Required]        
+        public int Id { get; set; }
+
+        [Required]      
+        public string Name { get; set; }
+
+        [Required]        
+        public string Surname { get; set; }
+
+        [Required]       
+        [Display(Name = "Email address")] 
+        public string Email { get; set; }
+
+        public bool HasPassword { get; set; }
+    }
+}
